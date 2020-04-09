4787b5130be1cd9ef68cee77bd2fcc90ef0a43cf
Mayumi <Kagura004@gmail.com>
2020-04-09 21:33:20 +0900

    add-message-sending-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,20 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.includes(:group)
   end
+
+  def create
+    message = Message.new(message_params)
+    if message.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: message.group.id)
+  end
+
 end
